6dbea3eb059f385869362696e691eede28e5a96d
Utkarsh Kaushik <ukaushik@hotmail.co.uk>
2023-02-16 17:39:44 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Bring in variables
+let btn = document.getElementById("button");
+const heading = document.getElementById("heading");
+
+// Declare default state
+let state = "light";
+
+// Initiate state change function when btn clicked
+btn.addEventListener("click", function () {
+  changeState();
+});
+
+// Change State function - if state is true, change to dark.
+const changeState = () => {
+  state == "light" ? darkMode() : lightMode();
+};
+
+const lightMode = () => {
+  state = "light";
+  heading.style.color = "black";
+  btn.style.color = "white";
+  btn.style.backgroundColor = "black";
+  btn.innerText = "Go Dark 🌙";
+  document.body.style.backgroundColor = "white";
+  console.log(state);
+};
+
+const darkMode = () => {
+  state = "dark";
+  heading.style.color = "white";
+  btn.style.backgroundColor = "white";
+  btn.innerText = "Go Light 🌞";
+  btn.style.color = "black";
+  document.body.style.backgroundColor = "black";
+  console.log(state);
+};
