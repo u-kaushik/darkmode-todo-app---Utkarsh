9c317c65ad7419a953316b3d94e51025b9829f00
Utkarsh Kaushik <ukaushik@hotmail.co.uk>
2023-02-21 10:46:20 +0000

    Updated individual li's to have two buttons. Can delete the first li but not others. Need to continue on functionality.

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
 // Bring in variables
-let btn = document.getElementById("button");
+let btn = document.getElementById("switch");
 const heading = document.getElementById("heading");
-const taskButton = document.querySelector(".task-input-button");
+const clearAll = document.querySelector(".task-input-clear");
+const addTask = document.querySelector(".task-input-add");
 let taskInput = document.querySelector(".task-input-text");
+const tasksList = document.querySelector(".tasks");
 let task = document.querySelectorAll("li");
 
 // Button disabled on default
-taskButton.disabled = true;
+addTask.disabled = true;
+clearAll.disabled = true;
 
 inputCheck = () => {
   taskInput.onkeyup = function () {
     taskInput.value.length > 2 && taskInput.value !== ""
-      ? (taskButton.disabled = false)
-      : (taskButton.disabled = true);
+      ? (addTask.disabled = false)
+      : (addTask.disabled = true);
   };
 };
-
 inputCheck();
 
-taskButton.addEventListener("click", function (e) {
-  let task = document.querySelector(".task-input-text").value;
+clearCheck = () => {
+  {
+    tasksList.children.length >= 0
+      ? (clearAll.disabled = false)
+      : (clearAll.disabled = true);
+  }
+};
+
+clearTask = () => {
+  const taskClear = document.querySelector(".task-clear");
+  taskClear.addEventListener("click", (e) => {
+    // let taskElement = document.querySelector("li");
+    console.log(taskClear.parentElement.parentElement);
+    taskClear.parentElement.parentElement.remove();
+    e.preventDefault();
+  });
+};
+
+addTask.addEventListener("click", function (e) {
+  let taskText = document.querySelector(".task-input-text").value;
   const tasksList = document.querySelector(".tasks");
   const taskElement = document.createElement("li");
+  const taskFunctions = document.createElement("div");
+  const taskClear = document.createElement("button");
+  const taskEdit = document.createElement("button");
+  taskFunctions.classList.add("task-functions");
   taskElement.classList.add("task");
-  taskElement.innerText = task;
+  taskClear.classList.add("task-clear");
+  taskEdit.classList.add("task-edit");
+  taskElement.innerText = taskText;
   tasksList.append(taskElement);
+  taskElement.append(taskFunctions);
+  taskFunctions.append(taskClear);
+  taskFunctions.append(taskEdit);
   taskInput.value = "";
-  taskButton.disabled = true;
+  addTask.disabled = true;
+  clearAll.disabled = true;
+  console.log(tasksList.children.length);
+  clearCheck();
+  clearTask();
+  e.preventDefault();
+});
+
+clearAll.addEventListener("click", (e) => {
+  const tasksList = document.querySelector(".tasks");
+  const confirmClear = () => {
+    const response = confirm("Are you sure you wanna clear all?");
+    if (response) {
+      tasksList.innerText = "";
+    }
+  };
+  confirmClear();
   e.preventDefault();
 });
 
